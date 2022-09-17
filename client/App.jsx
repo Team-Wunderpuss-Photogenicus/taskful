91cf289b5b05d1f1a1fedfab26d5246a3a019e85
karcodes1 <Kevin@karcodes.com>
2022-09-17 10:42:56 -0400

    added some boilerplate

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Tasks from "./components/Tasks";
+
+function App() {
+    return (
+        <MainComponent />
+    );
+}
+
+export default App;
---
+++ client/components/GroceryList.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+//add fetch call functions into deconstructed object
+import {  } from '../services/user';
+
+const GroceryList = () => {
+
+}
+
+export default GroceryList;
